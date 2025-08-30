2b183746b64483a684c1df4bcd0627d7f14e3982
{:michael, :maysonet} <michaelmaysonet74@gmail.com>
2025-08-29 20:53:42 -0500

    Update mix.exs (#13)

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule PokedexApi.MixProject do
   def project do
     [
       app: :pokedex_api,
-      version: "1.0.0",
+      version: "1.1.0",
       elixir: "~> 1.18",
       start_permanent: Mix.env() == :prod or Mix.env() == :container,
       deps: deps() ++ container_deps() ++ dev_deps()
